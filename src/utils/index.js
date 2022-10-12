082258d89bc4bf6b649955484fccfb837bfa9ecb
Joanne Ho <joaho@adobe.com>
2022-10-12 12:22:07 -0700

    added some debug info

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,11 +17,15 @@ const LinkManager = (props) => {
 const ConstructURL = (props) => {
   if (!props) console.log("error....");
 
-  if (!props.parent && props.folder && props.screen)
+  if (!props.parent && props.folder && props.screen) {
+    // top navigation links, e.g. /content/dam/blackrock/entities/about-blackrock/about-us-top-menu
     return `${RootPaths[1]}/${props.folder}/${props.screen}`;
+  }
 
-  if (props.parent && props.folder && props.screen)
+  if (props.parent && props.folder && props.screen) {
+    // clicking on image list link on homepage, e.g. /content/dam/blackrock/entities/about-blackrock/about-us-folder/about-us-screen-fragment
     return `${RootPaths[1]}/${props.parent}/${props.folder}/${props.screen}`;
+  }
 };
 
 export { RootPaths, LinkManager, ConstructURL };
