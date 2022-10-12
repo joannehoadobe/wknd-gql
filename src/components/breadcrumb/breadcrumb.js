ffbc6cd7f730d7385555d92f129f82596ebb5017
Joanne Ho <joaho@adobe.com>
2022-10-12 12:26:17 -0700

    modified js for breadcrumb to only render if applicable... for the XF model/page.

--- src/components/breadcrumb/breadcrumb.js
+++ src/components/breadcrumb/breadcrumb.js
@@ -4,13 +4,17 @@ import "./breadcrumb.css";
 
 const Breadcrumb = () => {
   const props = useParams();
- 
+  // CUSTOM for Blackrock
+  const hasValue = props.parent || props.screen;
+  if(hasValue) {
   return <nav className="breadcrumb">
         <ol className="breadcrumb-list">
           <li className="breadcrumb-list-item">{props.parent}</li>
           <li className="breadcrumb-list-item">{props.screen}</li>
         </ol>
       </nav>;
+  }
+  return null;
 }
 
 export default Breadcrumb;
